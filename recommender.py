15a6670dc29aa6a37b795e7efb4d543b491a2aa3
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-06-17 11:41:42 -0500

    Temporarily only handle songs with one instrument layer

--- recommender.py
+++ recommender.py
@@ -14,9 +14,9 @@ class Recommender:
 
     def add(self, song):
         """Converts song into relevant data, then adds it to the library."""
-        raw_layers = self.isolator.isolate(song)
-        layers = raw_layers.map(self.transformer.layer_to_sheet)
-        dataframes = layers.map(self.interpreter.interpret)
+        # temporarily, only handle songs with one instrument layer
+        # layers = self.isolator.isolate(song).map(self.transformer.layer_to_sheet).map(self.interpreter.interpret)
+        layers = self.transformer.layer_to_sheet.map(self.interpreter.interpret)
         raise NotImplementedError
 
     def recommend(self, params):
