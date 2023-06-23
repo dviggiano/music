c74cb756e2ce61863e21f434a2d23d370a8cc765
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-06-23 11:41:47 -0500

    Fix add script

--- add.py
+++ add.py
@@ -1,21 +1,27 @@
-"""
-A script for adding several songs at a time.
-Depends on the API running, and this directory being populated with MP3 files.
-"""
-
 import os
 import requests
 
 URL = 'http://localhost:5000/add'
-songs = os.path.join(os.getcwd(), 'songs')
 
-for filename in os.listdir(songs):
-    path = os.path.join(songs, filename)
 
-    if filename.endswith('.mp3'):
-        with open(path, 'rb') as f:
-            files = {'file': f}
-            response = requests.post(URL, files=files)
+def add():
+    """
+    Adds several songs at a time. Depends on the API running
+    and the `songs` directory being populated with MP3 files.
+    """
+    songs = os.path.join(os.getcwd(), 'songs')
 
-        if response.status_code != 200:
-            print(f'Failed to upload song (response status {response.status_code}).')
+    for filename in os.listdir(songs):
+        path = os.path.join(songs, filename)
+
+        if filename.endswith('.mp3'):
+            with open(path, 'rb') as f:
+                files = {'file': f}
+                response = requests.post(URL, files=files)
+
+            if response.status_code != 200:
+                print(f'Failed to upload song (response status {response.status_code}).')
+
+
+if __name__ == '__main__':
+    add()
