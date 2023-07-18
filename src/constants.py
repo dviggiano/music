54d280c038c0bf9ac0ff3bf3bf5b4299da3d8ac9
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-07-18 12:22:08 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -2,4 +2,4 @@ SONG_DATA_FILENAME = 'song_data.csv'
 SONG_LIST_FILENAME = 'song_list.csv'
 USER_DATA_FILENAME = 'users.csv'
 MODEL_FILENAME = 'model.sav'
-N_CLUSTERS = 8
+N_CLUSTERS = 8  # TODO optimize with silhouette score
